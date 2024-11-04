815bdc843e6032e278620df82620cf92bb25a3e4
sxakzc <51824214+sxakzc@users.noreply.github.com>
2024-11-05 00:45:24 +0800

    第一次提交，建立仓库。

---
+++ ConsoleApp/Data/BookRepository.cs
@@ -0,0 +1,91 @@
+// ConsoleApp/Data/BookRepository.cs
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using ConsoleApp.Models;
+
+namespace ConsoleApp.Data
+{
+    public class BookRepository
+    {
+        private readonly string _filePath;
+
+        public BookRepository()
+        {
+            // 获取项目根目录
+            var projectRoot = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+            _filePath = Path.Combine(projectRoot, "books.txt");
+        }
+
+        public List<Book> GetAllBooks()
+        {
+            if (!File.Exists(_filePath))
+            {
+                return new List<Book>();//为什么要返回一个空的List<book>
+            }
+
+            var lines = File.ReadAllLines(_filePath);
+            var books = new List<Book>();
+
+            foreach (var line in lines)
+            {
+                var parts = line.Split(',');
+                if (parts.Length == 4)
+                {
+                    var book = new Book
+                    {
+                        Id = int.Parse(parts[0]),
+                        Title = parts[1],
+                        Author = parts[2],
+                        ISBN = parts[3]
+                    };
+                    books.Add(book);
+                }
+            }
+
+            return books;
+        }
+
+        public void AddBook(Book book)
+        {
+            var books = GetAllBooks();
+            book.Id = books.Count + 1;
+            books.Add(book);
+
+            SaveBooks(books);
+        }
+
+        public void UpdateBook(Book book)
+        {
+            var books = GetAllBooks();
+            var existingBook = books.FirstOrDefault(b => b.Id == book.Id);
+            if (existingBook != null)
+            {
+                existingBook.Title = book.Title;
+                existingBook.Author = book.Author;
+                existingBook.ISBN = book.ISBN;
+
+                SaveBooks(books);
+            }
+        }
+
+        public void DeleteBook(int id)
+        {
+            var books = GetAllBooks();
+            var bookToDelete = books.FirstOrDefault(b => b.Id == id);
+            if (bookToDelete != null)
+            {
+                books.Remove(bookToDelete);
+
+                SaveBooks(books);
+            }
+        }
+
+        private void SaveBooks(List<Book> books)
+        {
+            var lines = books.Select(book => $"{book.Id},{book.Title},{book.Author},{book.ISBN}");
+            File.WriteAllLines(_filePath, lines);
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,121 @@
+﻿// ConsoleApp/Program.cs
+using System;
+using ConsoleApp.Data;
+using ConsoleApp.Models;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bookRepository = new BookRepository();
+                
+            while (true)
+            {
+                Console.WriteLine("图书管理系统");
+                Console.WriteLine("1. 添加图书");
+                Console.WriteLine("2. 查看所有图书");
+                Console.WriteLine("3. 更新图书");
+                Console.WriteLine("4. 删除图书");
+                Console.WriteLine("5. 退出");
+                Console.Write("请选择操作: ");
+                var choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        AddBook(bookRepository);
+                        break;
+                    case "2":
+                        ViewAllBooks(bookRepository);
+                        break;
+                    case "3":
+                        UpdateBook(bookRepository);
+                        break;
+                    case "4":
+                        DeleteBook(bookRepository);
+                        break;
+                    case "5":
+                        return;
+                    default:
+                        Console.WriteLine("无效的选择，请重新输入。");
+                        break;
+                }
+            }
+        }
+
+        static void AddBook(BookRepository bookRepository)
+        {
+            Console.Write("请输入书名: ");
+            var title = Console.ReadLine();
+            Console.Write("请输入作者: ");
+            var author = Console.ReadLine();
+            Console.Write("请输入ISBN: ");
+            var isbn = Console.ReadLine();
+
+            var book = new Book { Title = title, Author = author, ISBN = isbn };
+            bookRepository.AddBook(book);
+            Console.WriteLine("图书添加成功。");
+        }
+
+        static void ViewAllBooks(BookRepository bookRepository)
+        {
+            var books = bookRepository.GetAllBooks();
+            if (books.Count == 0)
+            {
+                Console.WriteLine("没有图书记录。");
+            }
+            else
+            {
+                foreach (var book in books)
+                {
+                    Console.WriteLine($"ID: {book.Id}, 书名: {book.Title}, 作者: {book.Author}, ISBN: {book.ISBN}");
+                }
+            }
+        }
+
+        static void UpdateBook(BookRepository bookRepository)
+        {
+            Console.Write("请输入要更新的图书ID: ");
+            if (int.TryParse(Console.ReadLine(), out int id))
+            {
+                var book = bookRepository.GetAllBooks().FirstOrDefault(b => b.Id == id);
+                if (book != null)
+                {
+                    Console.Write("请输入新的书名: ");
+                    book.Title = Console.ReadLine();
+                    Console.Write("请输入新的作者: ");
+                    book.Author = Console.ReadLine();
+                    Console.Write("请输入新的ISBN: ");
+                    book.ISBN = Console.ReadLine();
+
+                    bookRepository.UpdateBook(book);
+                    Console.WriteLine("图书更新成功。");
+                }
+                else
+                {
+                    Console.WriteLine("未找到指定的图书。");
+                }
+            }
+            else
+            {
+                Console.WriteLine("无效的ID。");
+            }
+        }
+
+        static void DeleteBook(BookRepository bookRepository)
+        {
+            Console.Write("请输入要删除的图书ID: ");
+            if (int.TryParse(Console.ReadLine(), out int id))
+            {
+                bookRepository.DeleteBook(id);
+                Console.WriteLine("图书删除成功。");
+            }
+            else
+            {
+                Console.WriteLine("无效的ID。");
+            }
+        }
+    }
+}
